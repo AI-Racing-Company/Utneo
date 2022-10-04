7c667db3b0ba8fb2950618996ac6447bcff7dbf5
Plat00n <70469468+DefinitlyNotABot@users.noreply.github.com>
2022-10-04 12:34:13 +0200

    Now able to disconnect properly; rezising doesn't hide disconnect button

--- Scripts/client_scene.gd
+++ Scripts/client_scene.gd
@@ -7,6 +7,7 @@ var is_connected = false
 puppet var my_id = null
 
 var playerPos = {}
+var player_IDs = []
 
 func _ready():
 	peer = NetworkedMultiplayerENet.new()
@@ -15,14 +16,20 @@ func _ready():
 	get_tree().network_peer = peer
 	print(get_tree().network_peer)
 	get_node("ClientText").text = "Connected To " + global.ip + ":" + str(global.port)
-	is_connected = true
 	get_tree().connect("connected_to_server", self, "connected_to_server")
 	get_tree().connect("connection_failed", self, "connection_failed")
+	get_tree().connect("server_disconnected", self, "serversided_disconnect")
+	get_viewport().connect("size_changed", self, "resized")
+
+
+func resized():
+	get_node("Disconnect").set_global_position(Vector2(get_viewport().get_visible_rect().size.x - 100,5))
 
 
 
 func connected_to_server():
 	print("am connected")
+	is_connected = true
 
 
 
@@ -38,6 +45,7 @@ func _physics_process(delta):
 puppet func player_pos_change(id, pos):
 	playerPos[str(id)] = pos
 	if get_parent().get_node("player_"+str(id)) == null:
+		player_IDs.append(id)
 		var player = preload("res://Prefabs/PlayerPrefab.tscn").instance()
 		player.set_name("player_"+ str(id))
 		get_parent().add_child(player)
@@ -47,20 +55,38 @@ puppet func player_pos_change(id, pos):
 puppet func client_connect(id):
 	var player = preload("res://Prefabs/PlayerPrefab.tscn").instance()
 	player.set_name("player_"+str(id))
+	player_IDs.append(id)
 	get_parent().add_child(player)
 
 puppet func client_disconnect(id):
 	var player = get_parent().get_node("player_"+str(id))
+	player_IDs.erase(player_IDs)
 	get_parent().remove_child(player)
 
 func _on_Disconnect_pressed():
 	get_tree().network_peer = null
 	peer.close_connection()
+	for i in player_IDs:
+		var player = get_parent().get_node("player_"+str(i))
+		get_parent().remove_child(player)
+	player_IDs.clear()
 	get_tree().change_scene("res://Scenes/TestScene.tscn")
+
+puppet func test_print():
+	print("test func")
+	
+func serversided_disconnect():
+	print("Server disconnected")
+	get_tree().network_peer = null
+	peer.close_connection()
+	for i in player_IDs:
+		var player = get_parent().get_node("player_"+str(i))
+		get_parent().remove_child(player)
+	player_IDs.clear()
+	get_tree().change_scene("res://Scenes/TestScene.tscn")
+
+
 
 func connection_failed():
 	print("Failed to connect to server...")
 	get_tree().change_scene("res://Scenes/TestScene.tscn")
-
-remote func test():
-	print("BigBoiiiiiiii")
--- Scripts/host_scene.gd
+++ Scripts/host_scene.gd
@@ -5,6 +5,8 @@ var peer = null
 var peer_id = 0
 var playerPos = {}
 
+var player_IDs = []
+
 #test
 
 func _ready():
@@ -12,12 +14,17 @@ func _ready():
 	get_node("HostText").text = "Hosting on " + global.ip + ":" + str(global.port)
 	peer = NetworkedMultiplayerENet.new()
 	peer.create_server(global.port, 5)
-	peer.COMPRESS_FASTLZ
+	peer.COMPRESS_ZLIB
 	get_tree().network_peer = peer
 	print(get_tree().get_network_peer())
 	print(get_tree().is_network_server())
 	get_tree().connect("network_peer_connected", self, "client_connect")
 	get_tree().connect("network_peer_disconnected", self, "client_disconnect")
+	get_viewport().connect("size_changed", self, "resized")
+
+
+func resized():
+	get_node("Disconnect").set_global_position(Vector2(get_viewport().get_visible_rect().size.x - 100,5))
 
 master func connection_established():
 	return true
@@ -25,6 +32,7 @@ master func connection_established():
 func client_connect(id):
 	print("connected player ID: ",id)
 	peer_id = id
+	player_IDs.append(id)
 	rset_id(id, "my_id", id)
 	var player = preload("res://Prefabs/PlayerPrefab.tscn").instance()
 	player.set_name("player_"+str(id))
@@ -33,6 +41,7 @@ func client_connect(id):
 	rpc("client_connect", id)
 
 func client_disconnect(id):
+	player_IDs.erase(id)
 	print("disconnected player ID: ",id)
 	var player = get_parent().get_node("player_"+str(id))
 	get_parent().remove_child(player)
@@ -50,7 +59,18 @@ master func getPos(id):
 
 
 func _on_Disconnect_pressed():
+	print("Disconnect all clients")
+	rpc("serversided_disconnect", true)
+	print("sent message to all connected clients")
+	
+	for i in player_IDs:
+		var player = get_parent().get_node("player_"+str(i))
+		get_parent().remove_child(player)
+	player_IDs.clear()
 	get_tree().network_peer = null
 	peer.close_connection()
+	
 	get_tree().change_scene("res://Scenes/TestScene.tscn")
+
+
 	
