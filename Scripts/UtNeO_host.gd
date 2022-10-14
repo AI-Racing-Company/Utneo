ad427abc1e14e0f9b34fba20c7367b609ebd9efb
Plat00n <70469468+DefinitlyNotABot@users.noreply.github.com>
2022-10-14 09:16:39 +0200

    More options
    Host can now:
     - End game
     - Continue game
     - Let someone win
    (not all yet implememted)
    Better resizeablility on host

--- Scripts/UtNeO_host.gd
+++ Scripts/UtNeO_host.gd
@@ -7,6 +7,8 @@ var player_IDs = []
 var player_names = {}
 var players_ignore = []
 
+var win = [false, false] # 0= Player won, 1 = Continue after player win
+
 var current_card = 0
 var game_started = false
 var current_player = 0
@@ -22,6 +24,7 @@ var r_t = 60 # round time
 onready var timer = get_node("Timer")
 
 func _ready():
+	get_viewport().connect("size_changed", self, "resized")
 	get_node("HostText").text = "Hosting on " + global.ip + ":" + str(global.port)
 	get_node("ClientConnect").text = "Connected Clients: 0"
 	peer = NetworkedMultiplayerENet.new()
@@ -31,6 +34,14 @@ func _ready():
 	get_tree().connect("network_peer_connected", self, "client_connect")
 	get_tree().connect("network_peer_disconnected", self, "client_disconnect")
 	#get_viewport().connect("size_changed", self, "resized")
+
+func resized():
+	var x = get_viewport().get_visible_rect().size.x
+	var y = get_viewport().get_visible_rect().size.y
+	get_node("Button").set_global_position(Vector2(x-250,0))
+	get_node("win").set_global_position(Vector2(x-250,75))
+	get_node("Continue").set_global_position(Vector2(x-250,150))
+	get_node("End").set_global_position(Vector2(x-250,225))
 
 func client_connect(id):
 	player_IDs.append(id)
@@ -52,92 +63,82 @@ func client_disconnect(id):
 
 
 master func add_card(id):
-	if current_player == id:
-		rand = rnd.randi_range(0,9)
+	if !win[0] || (win[0] && win[1]):
+		if current_player == id:
+			rand = rnd.randi_range(0,9)
 
-		all_cards.append(rnd)
-		var player_id = player_IDs.find(id,0)
-		player_cards[player_id].append(rand)
+			all_cards.append(rnd)
+			var player_id = player_IDs.find(id,0)
+			player_cards[player_id].append(rand)
 
-		rpc_id(id, "master_add_card", rand)
-		next_player()
-	else:
-		print("not your turn")
+			rpc_id(id, "master_add_card", rand)
+			next_player()
+		else:
+			print("not your turn")
 
 	
 master func cards_pushed(id, ops):
-	if current_player == id && players_ignore.find(id) == 0:
-		if ops[2] == "":
-			var c1 = int(ops[1])
-			var player_id = player_IDs.find(id)
-			if player_cards[player_id].find(c1) >= 0:
-				if c1 == current_card:
-					rpc_id(id, "card_removed")
-					player_cards[player_id].erase(c1)
-					current_card = c1
-					rpc("set_current_card", current_card)
-					set_client_text()
-					next_player()
-		else:
-			print("Player-ID: " + str(id))
-			var player_id = player_IDs.find(id)
-			print("player in array ID:" + str(player_id))
-			var op = ops[0]
-			var c1 = int(ops[1])
-			var c2 = int(ops[2])
-			var ex1 = player_cards[player_id].find(c1)
-			var ex2 = player_cards[player_id].find(c2)
-			print("Card 1 in array: " + str(ex1))
-			print("Card 2 in array: " + str(ex2))
-			if ex2 >= 0 && ex2 >= 0:
-				print(player_cards[player_id].count(c1))
-				if c1 == c2 && player_cards[player_id].count(c1) < 2:
-					return null
-				print("move possible")
-				var res = -1
-				match op:
-					" + ":
-						res = str(int(c1 + c2))
-						res = res[res.length()-1]
-						print(res)
-					" - ":
-						res = c1-c2
-						print(res)
-					" * ":
-						res = str(int(c1)*int(c2))
-						res = res[res.length()-1]
-						print(res)
-					" / ":
-						res = str(int(float(c1)/c2))
-						print(res)
-					" ^ ":
-						res = str(pow(c1,c2))
-						res = res[res.length()-1]
-						print(res)
-					" √ ":
-						res = pow(c2,float(1)/c1)
-						print(res)
-				if int(res) == current_card:
-					rpc_id(id, "card_removed")
-					player_cards[player_id].erase(c1)
-					player_cards[player_id].erase(c2)
-					print(str(player_cards[player_id].size()) + "Cards on hand")
-					if(player_cards[player_id].size() == 0):
-						player_won(id)
-					current_card = c2
-					rpc("set_current_card", current_card)
-					set_client_text()
-					next_player()
-
+	if !win[0] || (win[0] && win[1]):
+		if current_player == id && players_ignore.find(id) == 0:
+			if ops[2] == "":
+				var c1 = int(ops[1])
+				var player_id = player_IDs.find(id)
+				if player_cards[player_id].find(c1) >= 0:
+					if c1 == current_card:
+						rpc_id(id, "card_removed")
+						player_cards[player_id].erase(c1)
+						current_card = c1
+						rpc("set_current_card", current_card)
+						set_client_text()
+						next_player()
 			else:
-				print("clientside cards don't match serverside cards")
+				var player_id = player_IDs.find(id)
+				var op = ops[0]
+				var c1 = int(ops[1])
+				var c2 = int(ops[2])
+				var ex1 = player_cards[player_id].find(c1)
+				var ex2 = player_cards[player_id].find(c2)
+				if ex2 >= 0 && ex2 >= 0:
+					if c1 == c2 && player_cards[player_id].count(c1) < 2:
+						return null
+					var res = -1
+					match op:
+						" + ":
+							res = str(int(c1 + c2))
+							res = res[res.length()-1]
+						" - ":
+							res = c1-c2
+						" * ":
+							res = str(int(c1)*int(c2))
+							res = res[res.length()-1]
+						" / ":
+							res = str(int(float(c1)/c2))
+						" ^ ":
+							res = str(pow(c1,c2))
+							res = res[res.length()-1]
+						" √ ":
+							res = pow(c2,float(1)/c1)
+					if int(res) == current_card:
+						rpc_id(id, "card_removed")
+						player_cards[player_id].erase(c1)
+						player_cards[player_id].erase(c2)
+						if(player_cards[player_id].size() == 0):
+							player_won(id)
+						current_card = c2
+						rpc("set_current_card", current_card)
+						set_client_text()
+						next_player()
+
+				else:
+					print("clientside cards don't match serverside cards")
+		else:
+			print("not your turn or already won")
 	else:
-		print("not your turn or already won")
+		print("Someone, waiting on host to continue or end")
 
 func player_won(id):
 	players_ignore.append(id)
 	rpc("player_won", player_names[id])
-	print(id)
 
 func _physics_process(delta):
 	rand = rnd.randi()
@@ -166,8 +167,9 @@ func _on_Button_pressed():
 func next_player():
 	rpc_id(current_player, "endOfRound")
 	current_player = player_IDs[(player_IDs.find(current_player)+1)%player_IDs.size()]
-	while players_ignore.find(current_player):
-		current_player = player_IDs[(player_IDs.find(current_player)+1)%player_IDs.size()]
+	if players_ignore.size < player_IDs.size:
+		while players_ignore.find(current_player):
+			current_player = player_IDs[(player_IDs.find(current_player)+1)%player_IDs.size()]
 	rset("my_turn", false)
 	rset_id(current_player, "my_turn", true)
 	rpc_id(current_player, "startOfRound")
@@ -199,3 +201,11 @@ func set_client_text():
 
 func _on_win_pressed():
 	player_won(player_IDs[0])
+
+
+func _on_Contunue_pressed():
+	win[1] = true
+
+
+func _on_End_pressed():
+	game_end()
