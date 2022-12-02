691c9f647ba3757d64470c7f29e1b8cdcbe4ae09
Plat00n <70469468+DefinitlyNotABot@users.noreply.github.com>
2022-12-02 12:41:00 +0100

    Removed error with current card
    Hover / Click on current card throws no error when running via godot editor

--- Scripts/CardScript.gd
+++ Scripts/CardScript.gd
@@ -7,12 +7,15 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Card_pressed():
-	get_parent().get_parent().hand_card_pressed(self)
+	if self.name != "current_card":
+		get_parent().get_parent().hand_card_pressed(self)
 
 func _on_Card_mouse_entered():
-	if get_parent().get_parent().start_hover_above_card(self):
-		pass
+	if self.name != "current_card":
+		get_parent().get_parent().start_hover_above_card(self)
+		
 
 func _on_Card_mouse_exited():
-	if get_parent().get_parent().end_hover_above_card(self):
-		pass
+	if self.name != "current_card":
+		get_parent().get_parent().end_hover_above_card(self)
+		
