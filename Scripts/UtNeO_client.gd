27d95fcccbcdd548b3fc857fba1a43931a6c6e0b
Plat00n <70469468+DefinitlyNotABot@users.noreply.github.com>
2022-12-04 03:37:09 +0100

    changed client GUI, cards not yet

--- Scripts/UtNeO_client.gd
+++ Scripts/UtNeO_client.gd
@@ -53,7 +53,7 @@ func resized():
 	for i in range(len(my_card_nodes)):
 		var vec = Vector2((i+1)*add - 75/2,height-100)
 		my_card_nodes[i].set_global_position(vec)
-	get_node("Current Calculation").set_global_position(Vector2(width/2-75, height-225))
+	get_node("Current Calculation").set_global_position(Vector2(width/2-75, height-185))
 	if current_card_node != null:
 		current_card_node.set_global_position(Vector2(width/2-100,height/2-150))
 	get_node("Player List").set_global_position(Vector2(width-get_node("Player List").get_rect().size.x-5, 5))
@@ -61,12 +61,12 @@ func resized():
 	get_node("WinnerMessage").set_size(Vector2(width,50))
 
 	get_node("Current Player").set_global_position(Vector2(width/2-75, 5))
-	get_node("Timer/Time").set_global_position(Vector2(width/2-50, s_height - 205))
+	get_node("Timer/Time").set_global_position(Vector2(width/2-50, s_height - 165))
 
 	get_node("OverColorRect").set_size(Vector2(width,100))
 	get_node("OverColorRect").set_global_position(Vector2(0,height - 100 + overRectAdd))
-	timerRect.set_global_position(Vector2(0,s_height - 200))
-	get_node("past Calculations").set_global_position(Vector2(10,s_height - 360))
+	timerRect.set_global_position(Vector2(0,s_height - 160))
+	get_node("past Calculations").set_global_position(Vector2(10,s_height - 325))
 
 func add_card():
 	if !my_end:
